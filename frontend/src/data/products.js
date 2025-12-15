0c3f60649b54c33ca74cb5e599bea451596fba1e
shamsulhaquesami01 <2305055@ugrad.cse.buet.ac.bd>
2025-12-16 01:20:48 +0600

    frontend04

--- frontend/src/data/products.js
+++ frontend/src/data/products.js
@@ -38,5 +38,45 @@ export const products = [
     unit: "kg",
     image: "https://chaldn.com/_mpimage/aci-pure-salt-1-kg?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D47100&q=best&v=1&m=400&webp=1",
     stock: 200
+  },
+  {
+    id: 5,
+    name: "Fresh Eggs (1 Dozen)",
+    category: "Grocery",
+    price: 140,
+    originalPrice: 150,
+    unit: "12 pcs",
+    image: "https://chaldn.com/_mpimage/fresh-chicken-eggs-layer-12-pcs?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D103631&q=best&v=1",
+    stock: 50
+  },
+  {
+    id: 6,
+    name: "Liquid Milk (Pran)",
+    category: "Grocery",
+    price: 90,
+    originalPrice: 95,
+    unit: "1 ltr",
+    image: "https://chaldn.com/_mpimage/pran-uht-milk-1-ltr?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D72036&q=best&v=1",
+    stock: 40
+  },
+  {
+    id: 7,
+    name: "Bombay Sweets Potato Crackers",
+    category: "Food",
+    price: 25,
+    originalPrice: 25,
+    unit: "pack",
+    image: "https://chaldn.com/_mpimage/bombay-sweets-potato-crackers-22-gm?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D116843&q=best&v=1",
+    stock: 100
+  },
+   {
+    id: 8,
+    name: "Napa Extra",
+    category: "Pharmacy",
+    price: 25,
+    originalPrice: 30,
+    unit: "12 pcs",
+    image: "https://chaldn.com/_mpimage/napa-extra-50065-mg-tablet-12-pcs?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D117367&q=best&v=1",
+    stock: 100
   }
 ];
